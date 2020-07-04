9dacd7f81906af1b5824eb47b6f35d5272bcdb15
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-07-04 12:52:36 -0300

    Created class

---
+++ Types/Event/Event.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Fwc\Api\Type;
+
+class Event extends TypeAbstract implements TypeInterface
+{
+    protected $table = "event";
+    
+    protected $type = "Event";
+    
+    protected $properties = [ "name", "location", "startDate" ];
+    
+    protected $withTypes = [ "location" => "Place", "image" => "ImageObject" ];
+    
+    public function get(array $params): array 
+    {           
+        return parent::get($params);
+    }
+    
+    public function post(array $params): array 
+    {
+        return parent::post($params);
+    }
+    
+    public function put(string $id, $params): array 
+    {
+        return parent::put($id, $params);
+    }
+    
+    public function delete(string $id, $params): array 
+    {
+        return parent::delete($id, $params);
+    }
+    
+    public function createSqlTable($type = null) 
+    {
+        return parent::createSqlTable($type);
+    }
+}
