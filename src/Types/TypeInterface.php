fa21ebad3749debb85c098c1175b86666ed3ebda
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-08-21 16:30:46 -0300

    little adjusts on PHPDoc comments

--- src/Types/TypeInterface.php
+++ src/Types/TypeInterface.php
@@ -3,18 +3,35 @@
 namespace Plinct\Api\Type;
 
 interface TypeInterface 
-{   
+{
     /**
      * HTTP Request GET
-     * @param array $args
+     * @param array $params
+     * @return array
      */
     public function get(array $params): array;
-    
+
+    /**
+     * @param array $params
+     * @return array
+     */
     public function post(array $params): array;
-    
+
+    /**
+     * @param array $params
+     * @return array
+     */
     public function delete(array $params): array;
-    
+
+    /**
+     * @param array $params
+     * @return array
+     */
     public function put(array $params): array;
-    
+
+    /**
+     * @param null $type
+     * @return mixed
+     */
     public function createSqlTable($type = null);
 }
