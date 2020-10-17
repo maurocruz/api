506f0977ecc5ff4899106e2b46df062e86f36e94
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-10-17 13:33:30 -0300

    stylized psr

--- src/Types/Organization/Organization.php
+++ src/Types/Organization/Organization.php
@@ -5,6 +5,7 @@ namespace Plinct\Api\Type;
 use Plinct\Api\Server\Maintenance;
 
 use Plinct\Api\Server\Entity;
+use ReflectionException;
 
 class Organization extends Entity implements TypeInterface
 {
@@ -38,8 +39,7 @@ class Organization extends Entity implements TypeInterface
 
     /**
      * PUT
-     * @param string $id
-     * @param type $params
+     * @param array $params
      * @return array
  */
     public function put(array $params): array 
@@ -49,22 +49,23 @@ class Organization extends Entity implements TypeInterface
     
     /**
      * DELETE
-     * @param string $id
+     * @param array $params
      * @return array
      */
     public function delete(array $params): array 
     {
         return parent::delete($params);
     }
-    
+
     /**
      * CREATE SQL
-     * @param type $type
-     * @return type
+     * @param $type
+     * @return string
+     * @throws ReflectionException
      */
     public function createSqlTable($type = null) 
     {
-        $maintenance = (new Maintenance($this->request));
+        $maintenance = new Maintenance();
         $message[] = $maintenance->createSqlTable("ContactPoint");
         $message[] = $maintenance->createSqlTable("PostalAddress");
         $message[] = $maintenance->createSqlTable("ImageObject");
