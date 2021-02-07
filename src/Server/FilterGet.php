e72a65a37909dc6609c5a265727b1b33838bb295
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-07 12:33:54 -0300

    little adjusts

--- src/Server/FilterGet.php
+++ src/Server/FilterGet.php
@@ -16,14 +16,12 @@ class FilterGet
     private $offset;
 
     private $table;
-    
     private $properties;
             
     public function __construct($queryParams, $table, $properties) 
     {        
         $this->table = $table;
         $this->properties = $properties;
-        
         if (!empty($queryParams)) {
             $this->setQueries($queryParams);
         }
@@ -33,90 +31,55 @@ class FilterGet
     {   
         // fieds
         $this->fields = $queryParams['fields'] ?? $this->fields;
-                
         // query params        
         foreach ($queryParams as $key => $value) {
-            
             $idname = "id".$this->table;
             if ($value == "id") {
                 $value = $idname;
-            }            
-            
+            }
             // ORDER BY
             if (stripos($key, "orderBy") !== false) {
                 $ordering = $queryParams['ordering'] ?? null;
                 $this->orderBy = stripos($ordering, 'rand') !== false ? "rand()" : $value." ". $ordering;
             }
-            
             // WHERE
-              // like
             $like = stristr($key,"like", true) !== false ? stristr($key,"like", true) : ($key == "q" || $key == "search" ? "name" : null);
             if ($like) {
                 $whereArray[] = "`$like` LIKE '%$value%'";
 
-            }
-            //
-            elseif (!in_array($key, $this->noWhere)) {
+            } elseif (!in_array($key, $this->noWhere)) {
                 if (strpos($value, "|") !== false) {
                     $whereArray[] = "(`$key`='".str_replace("|","' OR `$key`='",addslashes($value))."')";
                 } else {
                     $whereArray[] = "`$key`='".addslashes($value)."'";
                 }
             }
-            
             if ($key == "id") {
                 $whereArray[] = "`$idname`=$value";
             }
-            
             if (stripos($key, "where") !== false) {
                 $whereArray[] = "$value";
             }
         }
-        
         // WHERE
-        $this->where = isset($whereArray) ? implode(" AND ", $whereArray) : null;
-        
+        $this->where = isset($whereArray) ? implode(" AND ", $whereArray) : null;        
         // groupBy
         $this->groupBy = $queryParams['groupBy'] ?? null;
-                
         // limit
-        $this->limit = isset($queryParams['limit']) ? ($queryParams['limit'] !== "none" ? $queryParams['limit'] : null) : $this->limit; 
-        
+        $this->limit = isset($queryParams['limit']) ? ($queryParams['limit'] !== "none" ? $queryParams['limit'] : null) : $this->limit;         
         // offset        
         $this->offset = $queryParams['offset'] ?? null;
-        
-        // properties
-        if (isset($queryParams['properties'])) {
-            $this->propertiesMerge($queryParams['properties']);
-        }
     }
     
-    /*public function field(): string
-    {
-        return $this->fields;
-    }*/
-
     public function where()
     {
         return $this->where;
     }
     
-    /*public function groupBy()
-    {
-        return $this->groupBy;
-    }*/
-    
     public function orderBy() 
     {
         return $this->orderBy;
     }
-    
-    /*public function ordering(): string
-    {
-        return stripos($this->ordering,'asc') !== false ? "Ascending" : 
-                ( stripos($this->ordering,"desc") !== false ? "Descending" : 
-                (stripos($this->ordering, 'rand') !== false ? "Randomly" : "Unordering") );     
-    }*/
     
     public function limit(): ?int
     {    
@@ -130,16 +93,6 @@ class FilterGet
     public function getProperties(): ?array
     {
         return $this->properties;
-    }
-    
-    private function propertiesMerge(string $propertiesIncrement) 
-    {
-        $propArray = explode(",", $propertiesIncrement);
-        foreach ($propArray as $value) {
-            $array[] = trim($value);
-        }
-
-        $this->properties = $array ? array_merge($this->properties, $array) : $this->properties;
     }
 
     public function getSqlStatement(): string
@@ -166,6 +119,4 @@ class FilterGet
     private function stmtOffset(): ?string {
         return $this->offset !== null ? " OFFSET $this->offset" : null;
     }
-
-
 }
