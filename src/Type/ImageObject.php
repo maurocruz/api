b9e015cf8a73c4a273fafc911cbc546fe2126ac3
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-17 15:53:57 -0300

    Added license in properties

--- src/Type/ImageObject.php
+++ src/Type/ImageObject.php
@@ -8,35 +8,22 @@ use Plinct\Tool\Thumbnail;
 use Plinct\Tool\StringTool;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
-use ReflectionException;
 
 class ImageObject extends Entity implements TypeInterface
 {
     protected $table = "imageObject";
-    
     protected $type = "ImageObject";
-    
-    protected $properties = [ "contentUrl", "caption", "keywords", "representativeOfPage", "position", "width", "height", "href" ];
-
+    protected $properties = [ "contentUrl", "caption", "keywords", "representativeOfPage", "position", "width", "height", "href", "license" ];
     protected $hasTypes = [ "author" => "Person" ];
 
-    /**
-     * PUT
-     * @param array $params
-     * @return array
-     */
-    public function get(array $params): array
-    {
+    public function get(array $params): array {
         $dataThumb = null;
         $dataSizes = null;
         $data = parent::getData($params);
-
         // EXTRA PROPERTIES
         if (!empty($data) && isset($params['properties']) && !array_key_exists('error', $data)) {
-            
             // THUMBNAIL
             if (strpos($params['properties'], "thumbnail") !== false) {
-                
                 foreach ($data as $valueThumb) { 
                     if (file_exists($_SERVER['DOCUMENT_ROOT'].$valueThumb['contentUrl'])) {
                         $thumbnail = new Thumbnail($valueThumb['contentUrl']);
@@ -47,37 +34,24 @@ class ImageObject extends Entity implements TypeInterface
                 }
                 $data = $dataThumb;
             }
-        
             // SIZES
             if (strpos($params['properties'], 'sizes') !== false) {
                 foreach ($data as $valueSizes) {
-                    
                     $imagePath = $_SERVER['DOCUMENT_ROOT'].$valueSizes['contentUrl'];
-                    
                     if (file_exists($imagePath) && is_file($imagePath)) {
-                        
                         list( $width, $height) = getimagesize($imagePath);
-                        
                         $valueSizes['width'] = $width;
                         $valueSizes['height'] = $height;
-                        
                         $dataSizes[] = $valueSizes;
                     }
                 }
                 $data = $dataSizes;
             }
         }
-        
         return parent::buildSchema($params, $data);
     }
-    
-    /**
-     * POST
-     * @param array $params
-     * @return array
-     */
-    public function post(array $params): array 
-    {
+
+    public function post(array $params): array {
         // upload image
         if (isset($_FILES['imageupload'])) {
             foreach ($_FILES['imageupload'] as $keyImagesUploaded => $valueImagesUploaded) {
@@ -96,9 +70,7 @@ class ImageObject extends Entity implements TypeInterface
                 $this->table = "imageObject";
                 parent::post($valueParams);
             }
-
             return [ "messagem" => "images uploaded" ];
-
         } elseif(isset($params['id']) && is_array($params['id'])) {
             foreach ($params['id'] as $valueParams) {
                 if (isset($params['tableHasPart'])) {
@@ -109,70 +81,44 @@ class ImageObject extends Entity implements TypeInterface
                 $this->table = "imageObject";
                 parent::post($newParams);
             }
-
             return [ "messagem" => "images uploaded" ];
-
         } else {
             return parent::post($params);
         }
     }
 
-    /**
-     * PUT
-     * @param array|null $params
-     * @return array
-     */
-    public function put(array $params = null): array 
-    {
+    public function put(array $params = null): array {
         return parent::put($params);
     }
 
-    /**
-     * DELETE
-     * @param array $params
-     * @return array
-     */
-    public function delete(array $params): array 
-    {
+    public function delete(array $params): array {
         $message = parent::delete($params);
-
         $imageFile = isset($params['contentUrl']) ? realpath($_SERVER['DOCUMENT_ROOT'].$params['contentUrl']) : null;
         if ($imageFile && $message["message"] == "Deleted successfully" && file_exists($imageFile)) {
             unlink($imageFile);
             $this->deleteThumbs($imageFile);
         }
-
         return $message;
     }
 
     // unlike thumbs folder
-    private function deleteThumbs($file)
-    {
+    private function deleteThumbs($file) {
         $folderThumbs = dirname($file)."/thumbs";
         if (is_dir($folderThumbs)) {
             $directory = new RecursiveDirectoryIterator($folderThumbs, FilesystemIterator::SKIP_DOTS);
             $iterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::CHILD_FIRST);
-
             foreach ($iterator as $file) {
                 $file->isDir() ? rmdir($file) : unlink($file);
             }
         }
     }
 
-    /**
-     * CREATE SQL
-     * @param null $type $type
-     * @return array
-     * @throws ReflectionException
-     */
-    public function createSqlTable($type = null): array
-    {
+    public function createSqlTable($type = null): array {
         $message[] =  parent::createSqlTable("ImageObject");
         return $message;
     }    
     
-    public static function getRepresentativeImageOfPage($data, $mode = "string")
-    {
+    public static function getRepresentativeImageOfPage($data, $mode = "string") {
         if ($data) {
             foreach ($data as $valueImage) {
                 if (isset($valueImage['representativeOfPage']) && $valueImage['representativeOfPage'] == true) {
@@ -184,16 +130,11 @@ class ImageObject extends Entity implements TypeInterface
         return null;
     }
     
-    private static function uploadImage($imageUpload, $location): string
-    {
+    private static function uploadImage($imageUpload, $location): string {
         $dir = substr($location, 0, 1) == '/' ? $location : '/'.$location;
-        
         $filename = StringTool::removeAccentsAndSpaces($imageUpload['name']);
-        
-        $path = $dir."/".$filename; 
-        
+        $path = $dir."/".$filename;
         (new Thumbnail($imageUpload['tmp_name']))->uploadImage($path);
-        
         return $filename;
     }  
 }
