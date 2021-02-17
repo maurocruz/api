1ed8c42db1da9cf5b428915268f192b6a3329a7a
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-17 12:42:10 -0300

    Added publisher has types

--- src/Type/Article.php
+++ src/Type/Article.php
@@ -8,51 +8,39 @@ use Plinct\Api\Server\Maintenance;
 class Article extends Entity implements TypeInterface
 {
     protected $table = "article";
-    
     protected $type = "Article";
-    
-    protected $properties = [ "headline", "author", "datePublished", "publisher"];
-    
-    protected $hasTypes = [ "image" => "ImageObject", "author" => "Person" ];
+    protected $properties = [ "headline", "datePublished" ];
+    protected $hasTypes = [ "image" => "ImageObject", "author" => "Person", "publisher" => true ];
 
-    public function get(array $params): array 
-    {
+    public function get(array $params): array {
         return parent::get($params);
     }
     
-    public function post(array $params): array 
-    {
+    public function post(array $params): array {
         $params['dateCreated'] = date("Y-m-d H:i:s");
         $params['datePublished'] = $params['publishied'] == '1' ? date("Y-m-d H:i:s") : null;
-
         return parent::post($params);
     }
     
-    public function put(array $params): array 
-    {
+    public function put(array $params): array {
         if (isset($params['datePublished'])) {
             $params['datePublished'] =
                 $params['publishied'] == '1' && $params['datePublished'] == ''
                     ? date("Y-m-d H:i:s")
                     : ( $params['publishied'] == '0' ? null : $params['datePublished'] );
         }
-
         return parent::put($params);
     }
     
-    public function delete(array $params): array 
-    {
+    public function delete(array $params): array {
         return parent::delete($params);
     }
     
-    public function createSqlTable($type = null): array
-    {
+    public function createSqlTable($type = null): array {
         $maintenance = new Maintenance();
-        
         $maintenance->createSqlTable("Person");
         $maintenance->createSqlTable("ImageObject");
         $maintenance->createSqlTable("Organization");
-        
         return parent::createSqlTable("Article");
     }
 }
