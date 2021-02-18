d675cbe174aeb2872946792a99e7cea52ccfd58c
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-18 15:52:22 -0300

    Added property name at hasType from the relationship

--- src/Type/WebPageElement.php
+++ src/Type/WebPageElement.php
@@ -8,45 +8,33 @@ use Plinct\Api\Server\Maintenance;
 class WebPageElement extends Entity implements TypeInterface
 {
     protected $table = "webPageElement";
-    
     protected $type = "WebPageElement";
-    
     protected $properties = [ "name", "text", "position", "image", "identifier" ];
-    
-    protected $hasTypes = [ "image" => "ImageObject", "identifier" => "PropertyValue" ];
-    
-    public function get(array $params): array 
-    {
-        $params['orderBy'] = $params['orderBy'] ?? "position";
+    protected $hasTypes = [ "image" => "ImageObject", "identifier" => "PropertyValue", "webPage" => "WebPage" ];
 
+    public function get(array $params): array {
+        $params['orderBy'] = $params['orderBy'] ?? "position";
         return parent::get($params);
     }
     
-    public function post(array $params): array 
-    {
+    public function post(array $params): array {
         return parent::post($params);
     }
     
-    public function put($params): array 
-    {
+    public function put($params): array {
         return parent::put($params);
     }
     
-    public function delete($params): array 
-    {
+    public function delete($params): array {
         return parent::delete($params);
     }
     
-    public function createSqlTable($type = null): array
-    {
+    public function createSqlTable($type = null): array {
         $maintenance = new Maintenance();
-
         $message[] = $maintenance->createSqlTable("propertyValue");
         $message[] = $maintenance->createSqlTable("webPage");
         $message[] = $maintenance->createSqlTable("imageObject");
-
         $message[] = parent::createSqlTable("WebPageElement");
-
         return $message;
     }
 }
