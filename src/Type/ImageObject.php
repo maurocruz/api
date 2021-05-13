fc0d481a03765875e35097a2447f6eb7809565e9
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-13 11:58:21 -0300

    ImageObject.php - added function if thumbnail is on (requested)

--- src/Type/ImageObject.php
+++ src/Type/ImageObject.php
@@ -3,6 +3,8 @@ namespace Plinct\Api\Type;
 
 use FilesystemIterator;
 use Plinct\Api\Server\Entity;
+use Plinct\Tool\ArrayTool;
+use Plinct\Tool\Image\Image;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 
@@ -11,6 +13,42 @@ class ImageObject extends Entity implements TypeInterface {
     protected $type = "ImageObject";
     protected $properties = [ "*" ];
     protected $hasTypes = [ "author" => "Person" ];
+
+    public function get(array $params): array {
+        // vars
+        $thumbnail = $params['thumbnail'] ?? null;
+        $format = $params['format'] ?? null;
+        if ($thumbnail == "on") $params['properties'] = "*";
+        unset($params['thumbnail']);
+        // GET
+        $data = parent::get($params);
+        // THUMBNAIL ON
+        if ($thumbnail=='on') {
+            $itemList = $data['itemListElement'] ?? $data;
+            foreach ($itemList as $key => $value) {
+                $item = $format ? $value['item'] : $value;
+                if (!$item['thumbnail']) {
+                    $image = new Image($item['contentUrl']);
+                    $image->thumbnail("200");
+                    $contentSize = $image->getFileSize();
+                    $width = $image->getWidth();
+                    $height = $image->getHeight();
+                    $encodingFormat = $image->getEncodingFormat();
+                    $thumbnailData = $image->getThumbSrc();
+                    $data['itemListElement'][$key]['item']['contentSize'] = $contentSize;
+                    $data['itemListElement'][$key]['item']['width'] = $width;
+                    $data['itemListElement'][$key]['item']['height'] = $height;
+                    $data['itemListElement'][$key]['item']['encodingFormat'] = $encodingFormat;
+                    $data['itemListElement'][$key]['item']['thumbnail'] = $thumbnailData;
+                    // save data
+                    $id = ArrayTool::searchByValue($item['identifier'], "id")['value'];
+                    $newParams = [ "id" => $id, "contentSize" => $contentSize, "width" => $width, "height" => $height, "encodingFormat" => $encodingFormat, "thumbnail" => $thumbnailData ];
+                    parent::put($newParams);
+                }
+            }
+        }
+        return $data;
+    }
 
     public function put(array $params): array {
         unset($params['contentUrl']);
