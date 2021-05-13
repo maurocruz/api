3bcc8b8d70c44ff5f20e58a9cece668281d2bc6f
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-13 18:00:50 -0300

    AuthController.php - eliminate dependency to plinct/api

--- src/Auth/AuthController.php
+++ src/Auth/AuthController.php
@@ -2,9 +2,9 @@
 namespace Plinct\Api\Auth;
 
 use Firebase\JWT\JWT;
-use Plinct\Api\PlinctApi;
-use Plinct\Api\Type\PropertyValue;
-use Plinct\Api\Type\User;
+use Plinct\Cms\App;
+use Plinct\Cms\Server\Api;
+use Plinct\Tool\ArrayTool;
 
 class AuthController {
 
@@ -18,25 +18,27 @@ class AuthController {
         // INVALID EMAIL
         if ($email === false) return [ "data" => "Invalid email", "status" => "Access unauthorized" ];
         // GET DATA
-        $data = (new User())->get([ "properties" => "*", "email" => $email ]);
+        $data = Api::get('user', [ "properties" => "*", "email" => $email ]);
         // ERROR
         if(isset($data['error'])) return [ "data" => $data['error']['message'], "status" => "Error" ];
         // USER NOT EXISTS
-        elseif (empty($data)) return [ "data" => "User not exists", "status" => "Access unauthorized" ];
-        // USER AUTHORIZED
-        elseif (password_verify($password, $data[0]['password'])) {
+        if (empty($data)) return [ "data" => "User not exists", "status" => "Access unauthorized" ];
+        // USER EXISTS BUT NOT ADMIN
+        if ($data[0]['status'] != 1) return [ "data" => "User exists but not admin", "status" => "Access unauthorized" ];
+        // USER EXISTS
+        if (password_verify($password, $data[0]['password'])) {
             $value = $data[0];
             $payload = [
-                "iss" => PlinctApi::$ISSUER,
-                "exp" => time() + PlinctApi::$JWT_EXPIRE,
+                "iss" => App::getApiHost(),
+                "exp" => time() + App::getApiUserExpire(),
                 "name" => $value['name'],
                 "admin" => $value['status'] == 1,
-                "uid" => PropertyValue::extractValue($value['identifier'], "id")
+                "uid" => ArrayTool::searchByValue($value['identifier'], "id")['value']
             ];
-            return [ "data" => JWT::encode($payload, PlinctApi::$JWT_SECRET_API_KEY), "status" => "Access authorized" ];
+            return [ "data" => JWT::encode($payload, App::getApiSecretKey()), "status" => "Access authorized" ];
         }
         // USER NOT AUTHORIZED
-        else return [ "data" => "User exists", "status" => "Access unauthorized" ];
+        return [ "data" => "User exists", "status" => "Access unauthorized" ];
     }
 
     /**
@@ -44,7 +46,7 @@ class AuthController {
      * @return false|string
      */
     public function register(array $params) {
-        $responseData = (new User())->post($params);
+        $responseData = Api::post('user', $params);
         // Init application
         if (isset($responseData['error']['code']) && $responseData['error']['code'] == "42S02") {
             return false;
