fae155abc85b854a0cb391329c24ad61c46150b5
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-17 16:52:35 -0300

    AuthController.php - added possibility set iss and expire by remote user

--- src/Auth/AuthController.php
+++ src/Auth/AuthController.php
@@ -2,8 +2,8 @@
 namespace Plinct\Api\Auth;
 
 use Firebase\JWT\JWT;
-use Plinct\Cms\App;
-use Plinct\Cms\Server\Api;
+use Plinct\Api\PlinctApi;
+use Plinct\Api\Type\User;
 use Plinct\Tool\ArrayTool;
 
 class AuthController {
@@ -15,10 +15,12 @@ class AuthController {
     public function login($params): array {
         $email = filter_var($params['email'], FILTER_VALIDATE_EMAIL);
         $password = $params['password'] ?? null;
+        $iss = $params['iss'] ?? PlinctApi::$ISSUER;
+        $exp = $params['exp'] ?? PlinctApi::$JWT_EXPIRE;
         // INVALID EMAIL
         if ($email === false) return [ "data" => "Invalid email", "status" => "Access unauthorized" ];
         // GET DATA
-        $data = Api::get('user', [ "properties" => "*", "email" => $email ]);
+        $data = (new User())->get([ "properties" => "*", "email" => $email ]);
         // ERROR
         if(isset($data['error'])) return [ "data" => $data['error']['message'], "status" => "Error" ];
         // USER NOT EXISTS
@@ -29,13 +31,13 @@ class AuthController {
         if (password_verify($password, $data[0]['password'])) {
             $value = $data[0];
             $payload = [
-                "iss" => App::getApiHost(),
-                "exp" => time() + App::getApiUserExpire(),
+                "iss" => $iss,
+                "exp" => time() + $exp,
                 "name" => $value['name'],
                 "admin" => $value['status'] == 1,
                 "uid" => ArrayTool::searchByValue($value['identifier'], "id")['value']
             ];
-            return [ "data" => JWT::encode($payload, App::getApiSecretKey()), "status" => "Access authorized" ];
+            return [ "data" => JWT::encode($payload, PlinctApi::$JWT_SECRET_API_KEY), "status" => "Access authorized" ];
         }
         // USER NOT AUTHORIZED
         return [ "data" => "User exists", "status" => "Access unauthorized" ];
@@ -46,7 +48,7 @@ class AuthController {
      * @return false|string
      */
     public function register(array $params) {
-        $responseData = Api::post('user', $params);
+        $responseData = (new User())->post($params);
         // Init application
         if (isset($responseData['error']['code']) && $responseData['error']['code'] == "42S02") {
             return false;
