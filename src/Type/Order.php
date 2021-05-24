ec771373c8cf89e45ccb8ad9ce124ccff36587e8
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-24 19:23:03 -0300

    Order.php - Eliminated post in History

--- src/Type/Order.php
+++ src/Type/Order.php
@@ -12,17 +12,6 @@ class Order extends Entity implements TypeInterface {
     protected $properties = [ "*" ];
     protected $hasTypes = [ "history" => "History", "partOfInvoice" => "Invoice", "orderedItem" => "OrderItem", "customer" => true, "seller" => true ];
 
-    public function post(array $params): array {
-        $data = parent::post($params);
-        (new History())->postHistory("CREATED", _("Create new order"), "order", $data['id']);
-        return $data;
-    }
-    
-    public function put($params): array {
-        (new History())->postHistory("UPDATE", filter_input(INPUT_GET, "summaryHistory"), "order", $params['id']);
-        return parent::put($params);
-    }
-
     public function search($params, $nameLike): array {
         $orderBy = $params['orderBy'];
         $query = "select `order`.* from `order` left join `localBusiness` on `order`.customer=localBusiness.idlocalBusiness AND `order`.customerType='LocalBusiness' left join `organization` on `order`.customer=`organization`.idorganization AND `order`.customerType='Organization' left join `person` on `order`.customer=`person`.idperson AND `order`.customerType='Person' where (`localBusiness`.`name` like '%$nameLike%' OR `organization`.`name` like '%$nameLike%' OR `person`.`name` like '%$nameLike%') order by $orderBy;";
