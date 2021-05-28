3b68ad7fc55350c2e4beab186b3ab81a8c5412ad
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-28 15:55:33 -0300

    History.php - added properties user for default

--- src/Type/History.php
+++ src/Type/History.php
@@ -6,8 +6,8 @@ use Plinct\Api\Server\Entity;
 class History extends Entity implements TypeInterface {
     protected $table = "history";
     protected $type = "History";
-    protected $properties = [ "*" ];
-    protected $withTypes = [ "user" => "User" ];
+    protected $properties = [ "*", "user" ];
+    protected $hasTypes = [ "user" => "User" ];
     
     public function createSqlTable($type = null): array {
         return parent::createSqlTable("History");
