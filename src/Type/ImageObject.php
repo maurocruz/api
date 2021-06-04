5e5c0205f261ef1070c9f841a804fc93227848b2
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-06-04 16:35:35 -0300

    ImageObject.php - Enabled delete image;

--- src/Type/ImageObject.php
+++ src/Type/ImageObject.php
@@ -1,12 +1,9 @@
 <?php
 namespace Plinct\Api\Type;
 
-use FilesystemIterator;
 use Plinct\Api\Server\Entity;
 use Plinct\Tool\ArrayTool;
 use Plinct\Tool\Image\Image;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
 
 class ImageObject extends Entity implements TypeInterface {
     protected $table = "imageObject";
@@ -53,28 +50,6 @@ class ImageObject extends Entity implements TypeInterface {
     public function put(array $params): array {
         unset($params['contentUrl']);
         return parent::put($params);
-    }
-
-    public function delete(array $params): array {
-        $message = parent::delete([ "id" => $params['idIsPartOf'] ]);
-        $imageFile = isset($params['contentUrl']) ? realpath($_SERVER['DOCUMENT_ROOT'].$params['contentUrl']) : null;
-        if ($imageFile && $message["message"] == "Deleted successfully" && file_exists($imageFile)) {
-            unlink($imageFile);
-            $this->deleteThumbs($imageFile);
-        }
-        return $message;
-    }
-
-    // unlike thumbs folder
-    private function deleteThumbs($file) {
-        $folderThumbs = dirname($file)."/thumbs";
-        if (is_dir($folderThumbs)) {
-            $directory = new RecursiveDirectoryIterator($folderThumbs, FilesystemIterator::SKIP_DOTS);
-            $iterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::CHILD_FIRST);
-            foreach ($iterator as $file) {
-                $file->isDir() ? rmdir($file) : unlink($file);
-            }
-        }
     }
 
     public function createSqlTable($type = null): array {
