c3ab477ba241ee67d5b78d0e186d78043334ff62
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-06-29 15:30:39 -0300

    PropertyValue.php - Eliminated redundant functions

--- src/Type/PropertyValue.php
+++ src/Type/PropertyValue.php
@@ -7,35 +7,8 @@ class PropertyValue extends Entity implements TypeInterface {
     protected $table = "propertyValue";
     protected $type = "PropertyValue";
     protected $properties = [ "name", "value" ];
-
-    public function get(array $params): array {
-        return parent::get($params);
-    }
-    
-    public function post(array $params): array {
-        return parent::post($params);
-    }
-    
-    public function put(array $params): array {
-        return parent::put($params);
-    }
-    
-    public function delete(array $params): array {
-        return parent::delete($params);
-    }
     
     public function createSqlTable($type = null): array {
         return parent::createSqlTable("PropertyValue");
-    }
-        
-    public static function extractValue($array, $name): ?string {
-        if ($array) {
-            foreach ($array as $value) {
-                if ($value['name'] == $name) {
-                    return $value['value'] ?? $value['result'] ?? $value['description'] ?? null;
-                }
-            }
-        }
-        return null;
     }
 }
