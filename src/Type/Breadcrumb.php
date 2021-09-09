a0409defbf2fb04bf510b6c338454f7e70a11217
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-09-09 18:12:05 -0300

    Add PHPDOC

--- src/Type/Breadcrumb.php
+++ src/Type/Breadcrumb.php
@@ -1,12 +1,21 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Plinct\Api\Type;
 
 class Breadcrumb
 {
-    public function get($params): array {
+    /**
+     * @param $params
+     * @return array
+     */
+    public function get($params): array
+    {
         $urlArray = array_filter(explode("/",$params['url']));
         $key = count($urlArray);
         $items[] = self::item($key, $params['url'], $params['alternativeHeadline']);
+
         if ($key > 1) {
             end($urlArray);
             while(current($urlArray)) {
@@ -14,22 +23,36 @@ class Breadcrumb
                 if(!empty($urlArray)) {
                     $items[] = self::getNewParams($urlArray);
                     end($urlArray);
+
                 } else {
                     break;
                 }
             }
         }
+
         return array_reverse($items);
     }
 
-    private static function getNewParams($urlArray): array {
+    /**
+     * @param $urlArray
+     * @return array
+     */
+    private static function getNewParams($urlArray): array
+    {
         $parentUrl = DIRECTORY_SEPARATOR . implode("/", $urlArray);
         $newParams = ["url" => $parentUrl, "properties" => "alternativeHeadline"];
         $parentData = (new WebPage())->get($newParams);
         return self::item(count($urlArray), $parentUrl, $parentData[0]['alternativeHeadline']);
     }
 
-    private static function item($position, $url, $alternativeHeadline): array {
+    /**
+     * @param $position
+     * @param $url
+     * @param $alternativeHeadline
+     * @return array
+     */
+    private static function item($position, $url, $alternativeHeadline): array
+    {
         return [
             "position" => $position,
             "item" => [
