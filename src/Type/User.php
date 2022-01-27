11639400f13045f686b79d10c5675ff433e2d140
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-01-27 13:56:10 -0300

    Update return messages

--- src/Type/User.php
+++ src/Type/User.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Plinct\Api\Type;
 
 use Plinct\Api\Server\Entity;
+use Plinct\PDO\PDOConnect;
 use ReflectionException;
 
 class User extends Entity implements TypeInterface
@@ -29,37 +30,37 @@ class User extends Entity implements TypeInterface
     public function post(array $params): array
     {
         if (strlen($params['name']) < 5 ) {
-            return [ "error" => [
-                "message" => "The name must be longer than 4 characters"
-            ]];
+            return [ "status" => 'fail', "message" => "The name must be longer than 4 characters" ];
         }
+
         if (filter_var($params['email'], FILTER_VALIDATE_EMAIL) === false) {
-            return [ "error" => [
-                "message" => "Invalid email" 
-            ]];
+            return [ "status" => 'fail', "message" => "Invalid email" ];
         }
-        if(strlen($params['password']) < 8) {  
-            return [ "error" => [
-                "message" => "Password must be a minimum of 8 characters" 
-            ]];
+
+        if(strlen($params['password']) < 8) {
+            return [ "status" => 'fail', "message" => "Password must be a minimum of 8 characters" ];
         }
-        if(preg_match('@[A-Z]@', $params['password']) === 0) {  
-            return [ "error" => [
-                "message" => "Password must contain at least one uppercase character" 
-            ]];
+
+        if(preg_match('@[A-Z]@', $params['password']) === 0) {
+            return [ "status" => 'fail', "message" => "Password must contain at least one uppercase character" ];
         }
         if(preg_match('@[a-z]@', $params['password']) === 0) {  
-            return [ "error" => [
-                "message" => "Password must contain at least one lowercase character" 
-            ]];
+            return [ "status" => 'fail', "message" => "Password must contain at least one lowercase character" ];
         }
         if(preg_match('@[0-9]@', $params['password']) === 0) {  
-            return [ "error" => [
-                "message" => "Password must contain at least 1 number" 
-            ]];
+            return [ "status" => 'fail', "message" => "Password must contain at least 1 number" ];
         }
+
         $params['password'] = password_hash($params['password'], PASSWORD_DEFAULT);
-        return parent::created($params);
+
+        $data = parent::created($params);
+
+        if (isset($data['error']) || (isset($data['status']) && $data['status'] == 'error')) {
+            return [ 'status' => 'error', 'data' => $data ];
+        } else {
+            $id = PDOConnect::lastInsertId();
+            return ['id'=>$id];
+        }
     }
 
     /**
