1045d893dd21f59631bb40b433dfe93c72efb9c8
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-03-15 12:11:11 -0300

    Id is required if there is a hasType property

--- src/Server/GetData/GetDataAbstract.php
+++ src/Server/GetData/GetDataAbstract.php
@@ -8,128 +8,136 @@ use Plinct\PDO\PDOConnect;
 
 class GetDataAbstract
 {
-    /**
-     * @var string
-     */
-    protected string $query;
-    /**
-     * @var string
-     */
-    protected string $fields = '*';
-    /**
-     * @var string
-     */
-    protected string $table;
-    /**
-     * @var array
-     */
-    protected array $params = [];
-    /**
-     * @var bool|array
-     */
-    protected $error = false;
-    /**
-     * @var string
-     */
-    protected string $limit = '200';
+  /**
+   * @var string
+   */
+  protected string $query;
+  /**
+   * @var string
+   */
+  protected string $fields = '*';
+  /**
+   * @var string
+   */
+  protected string $table;
+  /**
+   * @var array
+   */
+  protected array $params = [];
+  /**
+   * @var bool|array
+   */
+  protected $error = false;
+  /**
+   * @var string
+   */
+  protected string $limit = '200';
 
-    /**
-     * @param string $limit
-     */
-    public function setLimit(string $limit)
-    {
-        $this->limit = $limit;
+  /**
+   * @param string $limit
+   */
+  public function setLimit(string $limit)
+  {
+    $this->limit = $limit;
+  }
+
+  /**
+   *
+   */
+  public function setQuery()
+  {
+    $this->query = "SELECT $this->fields FROM `$this->table`";
+  }
+
+  /**
+   * @param mixed $params
+   */
+  public function setParams($params): void
+  {
+    if (isset($params['limit'])) {
+      $this->limit = (string) $params['limit'];
+      unset($params['limit']);
+      }
+    $this->params = $params;
+  }
+
+  /**
+   */
+  protected function setFields()
+  {
+    if (isset($this->params['fields'])) {
+			$fields = $this->params['fields'];
+			unset($this->params['fields']);
+
+			// ID IS REQUIRED IF THERE IS A hasType PROPERTY
+			$idname = "id$this->table";
+			if (strpos($fields,$idname) === false) {
+				$fields .= ",$idname";
+			}
+
+      $this->fields = $fields;
+    }
+  }
+
+  /**
+   *
+   */
+  protected function parseParams()
+  {
+    $groupBy = $this->params['groupBy'] ?? null;
+    $orderBy = $this->params['orderBy'] ?? null;
+    $ordering = $this->params['ordering'] ?? null;
+    $offset = $this->params['offset'] ?? null;
+
+    // GROUP BY
+    if ($groupBy) $this->query .= " GROUP BY $groupBy";
+
+    // ORDER BY
+    if ($orderBy) $this->query .= " ORDER BY $orderBy $ordering";
+
+    // OFFSET
+    if ($offset) $this->query .= " OFFSET $offset";
+  }
+
+  /**
+   *
+   */
+  protected function parseWhereClause()
+  {
+    $where = null;
+
+    foreach ($this->params as $key => $value) {
+      // WHERE
+      if ($key == 'where') $where[] = $value;
+      // ID
+      if ($key == 'id') {
+        $idname = "id$this->table";
+        $where[] = "`$idname`=$value";
+      }
+      // LIKE
+      $like = stristr($key,"like", true);
+      if ($like) {
+        $where[] = "LOWER(REPLACE(`$like`,' ','')) LIKE LOWER(REPLACE('%$value%',' ',''))";
+      }
     }
 
-    /**
-     *
-     */
-    public function setQuery()
-    {
-        $this->query = "SELECT $this->fields FROM `$this->table`";
-    }
+    //
+    $columnsTable = PDOConnect::run("SHOW COLUMNS FROM `$this->table`;");
 
-    /**
-     * @param mixed $params
-     */
-    public function setParams($params): void
-    {
-        if (isset($params['limit'])) {
-            $this->limit = (string) $params['limit'];
-            unset($params['limit']);
+    if (isset($columnsTable['error'])) {
+      $this->error = $columnsTable;
+
+    } else {
+      foreach ($columnsTable as $value) {
+        $field = $value['Field'];
+        $valueField = $this->params[$field] ?? null;
+        if ($valueField) {
+          $fieldValue = is_string($valueField) ? addslashes($valueField) : $valueField;
+          $where[] = "`$field`='$fieldValue'";
         }
-        $this->params = $params;
+      }
+
+      $this->query .= $where ? " WHERE " . implode(" AND ", $where) : null;
     }
-
-    /**
-     */
-    protected function setFields()
-    {
-        if (isset($this->params['fields'])) {
-            $this->fields = $this->params['fields'];
-            unset($this->params['fields']);
-        }
-    }
-
-    /**
-     *
-     */
-    protected function parseParams()
-    {
-        $groupBy = $this->params['groupBy'] ?? null;
-        $orderBy = $this->params['orderBy'] ?? null;
-        $ordering = $this->params['ordering'] ?? null;
-        $offset = $this->params['offset'] ?? null;
-
-        // GROUP BY
-        if ($groupBy) $this->query .= " GROUP BY $groupBy";
-
-        // ORDER BY
-        if ($orderBy) $this->query .= " ORDER BY $orderBy $ordering";
-
-        // OFFSET
-        if ($offset) $this->query .= " OFFSET $offset";
-    }
-
-    /**
-     *
-     */
-    protected function parseWhereClause()
-    {
-        $where = null;
-
-        foreach ($this->params as $key => $value) {
-            // WHERE
-            if ($key == 'where') $where[] = $value;
-            // ID
-            if ($key == 'id') {
-                $idname = "id$this->table";
-                $where[] = "`$idname`=$value";
-            }
-            // LIKE
-            $like = stristr($key,"like", true);
-            if ($like) {
-                $where[] = "LOWER(REPLACE(`$like`,' ','')) LIKE LOWER(REPLACE('%$value%',' ',''))";
-            }
-        }
-
-        //
-        $columnsTable = PDOConnect::run("SHOW COLUMNS FROM `$this->table`;");
-
-        if (isset($columnsTable['error'])) {
-            $this->error = $columnsTable;
-
-        } else {
-            foreach ($columnsTable as $value) {
-                $field = $value['Field'];
-                $valueField = $this->params[$field] ?? null;
-                if ($valueField) {
-                    $fieldValue = is_string($valueField) ? addslashes($valueField) : $valueField;
-                    $where[] = "`$field`='$fieldValue'";
-                }
-            }
-
-            $this->query .= $where ? " WHERE " . implode(" AND ", $where) : null;
-        }
-    }
+  }
 }
